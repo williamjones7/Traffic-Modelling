f14bed66ceb57d05b8dd0d6502e65046adf1a36d
AidanMassaro <98093381+AidanMassaro@users.noreply.github.com>
2022-11-08 15:51:51 +0000

    Update Traffic Networks.py

--- Traffic Networks.py
+++ Traffic Networks.py
@@ -6,7 +6,19 @@ class car:
         self.position = initial_position
         self.v = initial_velocity
     
-    #def accelerate(v):
+    def accelerate():
+        
     
     def move():
         self.position += self.v
+        
+        
+class road:
+    def __init__(length, density):
+        self.cars = []
+        self.build_road()
+        
+    def build_road():
+        for position in range(length):
+            if 
+            
