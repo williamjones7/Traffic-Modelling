a6f81dd88ea6164ef419e963ccc888752dad2620
AidanMassaro <98093381+AidanMassaro@users.noreply.github.com>
2022-11-08 15:32:21 +0000

    Update Traffic Networks.py

---
+++ Traffic Networks.py
@@ -0,0 +1,12 @@
+import random
+import numpy as np
+
+class car:
+    def __init__(initial_position, initial_velocity):
+        self.position = initial_position
+        self.v = initial_velocity
+    
+    #def accelerate(v):
+    
+    def move():
+        self.position += self.v
